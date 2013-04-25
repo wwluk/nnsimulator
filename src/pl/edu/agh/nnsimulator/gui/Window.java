de24bde667bed22cf4b436d038f3c7d0d7106be4
gzuber <zubijaw@gmail.com>
2013-04-26 00:02:11 +0200

    Zaimplementowane GUI dla 2. części projektu

--- src/pl/edu/agh/nnsimulator/gui/Window.java
+++ src/pl/edu/agh/nnsimulator/gui/Window.java
@@ -1,12 +1,13 @@
 package pl.edu.agh.nnsimulator.gui;
 
 import au.com.bytecode.opencsv.CSVReader;
-import pl.edu.agh.nnsimulator.NeuralNetwork;
-import pl.edu.agh.nnsimulator.activationFunctions.ActivationFunctionType;
+import pl.edu.agh.nnsimulator.KohonenNetwork;
+import pl.edu.agh.nnsimulator.LearningParameters;
 import pl.edu.agh.nnsimulator.exceptions.TooMuchInputLayersException;
 import pl.edu.agh.nnsimulator.layers.InvalidDimensionsException;
-import pl.edu.agh.nnsimulator.layers.NetworkLayer;
-import pl.edu.agh.nnsimulator.neurons.NeuronData;
+import pl.edu.agh.nnsimulator.weightsInitializers.RandomWeightsInitializer;
+import pl.edu.agh.nnsimulator.weightsInitializers.WeightsInitializer;
+import pl.edu.agh.nnsimulator.weightsInitializers.ZeroWeightsInitializer;
 
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
@@ -17,28 +18,33 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 import javax.swing.*;
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
-
-import static javax.swing.JScrollBar.*;
 
 public class Window {
 
     private JFrame frame;
-    private JTextField _layersCount;
+    private JTextField _inputCount;
     private List<MyFrame> _myFrames = new LinkedList<MyFrame>();
-    private Box _scrollPane;
     private JButton _countButton;
     private File _fileChoosenToImport;
     private JButton _btnChooseImportingFile;
-    private JScrollPane  _jScrollPane;
     private JTextArea _inputTextArea;
     private JTextArea _outputTextArea;
+    private JLabel label;
+    private JTextField _columnsTextField;
+    private JTextField _rowsTextField;
+    private ButtonGroup _randomOrCustomWeights;
+    private JPanel _randomOrCustomJPanel;
+    private JTextField _minTextField;
+    private JTextField _maxTextField;
+    private JTextField textField;
+    private JTextField _alfa;
+    private JTextField _neighbourhood;
+    private JTextField _iterationCount;
+    private KohonenNetwork _kohonenNetwork;
 
     /**
      * Launch the application.
@@ -72,16 +78,87 @@ public class Window {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().setLayout(null);
 
-        JLabel lblLayersCount = new JLabel("Layers Count");
-        lblLayersCount.setBounds(0, 5, 114, 30);
-        frame.getContentPane().add(lblLayersCount);
+        JLabel lblInputCount = new JLabel("Input count");
+        lblInputCount.setBounds(0, 5, 114, 30);
+        frame.getContentPane().add(lblInputCount);
 
-        _layersCount = new JTextField();
+        _inputCount = new JTextField();
+        _inputCount.setText("2");
+        _inputCount.setBounds(96, 12, 32, 19);
+        frame.getContentPane().add(_inputCount);
+        _inputCount.setColumns(10);
 
-        _layersCount.setText("2");
-        _layersCount.setBounds(96, 12, 32, 19);
-        frame.getContentPane().add(_layersCount);
-        _layersCount.setColumns(10);
+        JLabel lblColumns = new JLabel("Columns");
+        lblColumns.setBounds(0, 35, 114, 30);
+        frame.getContentPane().add(lblColumns);
+
+        _columnsTextField = new JTextField();
+        _columnsTextField.setText("2");
+        _columnsTextField.setBounds(76, 42, 32, 19);
+        frame.getContentPane().add(_columnsTextField);
+        _columnsTextField.setColumns(10);
+
+        JLabel lblRows = new JLabel("Rows");
+        lblRows.setBounds(200, 35, 114, 30);
+        frame.getContentPane().add(lblRows);
+
+        _rowsTextField = new JTextField();
+        _rowsTextField.setText("2");
+        _rowsTextField.setBounds(256, 42, 32, 19);
+        frame.getContentPane().add(_rowsTextField);
+        _rowsTextField.setColumns(10);
+
+        JLabel lblWeights = new JLabel("Weights");
+        lblWeights.setBounds(0, 75, 114, 30);
+        frame.getContentPane().add(lblWeights);
+
+
+
+
+        _randomOrCustomJPanel= new JPanel();
+        _randomOrCustomJPanel.setBounds(0,0,300,30);
+        _randomOrCustomWeights = new ButtonGroup();
+
+        JRadioButton jRadioButton = new JRadioButton("Zero", true);
+        jRadioButton.setActionCommand("Zero");
+        _randomOrCustomJPanel.add(jRadioButton);
+        _randomOrCustomWeights.add(jRadioButton);
+
+        JRadioButton jRadioButton2 = new JRadioButton("Random", false);
+        jRadioButton2.setActionCommand("Random");
+        _randomOrCustomJPanel.add(jRadioButton2);
+        _randomOrCustomWeights.add(jRadioButton2);
+
+        _randomOrCustomJPanel.setBounds(0, 100, 180, 30);
+        //_randomOrCustomJPanel.setSize(300,30);
+
+        _randomOrCustomJPanel.setVisible(true);
+        _randomOrCustomJPanel.revalidate();
+        //_randomOrCustomJPanel.set
+        frame.getContentPane().add(_randomOrCustomJPanel);
+
+
+
+        _minTextField = new JTextField();
+        _minTextField.setText("2");
+        _minTextField.setBounds(183, 107, 50, 20);
+        frame.getContentPane().add(_minTextField);
+
+        _maxTextField = new JTextField();
+        _maxTextField.setText("2");
+        _maxTextField.setBounds(260, 107, 50, 20);
+        frame.getContentPane().add(_maxTextField);
+
+        JLabel _pause = new JLabel("-");
+        _pause.setBounds(240, 107, 15, 20);
+        frame.getContentPane().add(_pause);
+
+
+
+
+
+        lblInputCount.setBounds(0, 5, 114, 30);
+        frame.getContentPane().add(lblInputCount);
 
         JLabel lblNeuronsInLayer = new JLabel("Neurons in layer count");
         lblNeuronsInLayer.setBounds(225, 45, 181, 15);
@@ -93,57 +170,28 @@ public class Window {
 
         JLabel lblLayerNumber = new JLabel("<html>Layer<br>number</html>");
         lblLayerNumber.setBounds(12, 45, 102, 30);
-        //frame.getContentPane().add(lblLayerNumber);
-
-        JLabel lblLayers = new JLabel("Layers (number, neurons, activation function)");
-        lblLayers.setBounds(12, 40, 330, 45);
-        frame.getContentPane().add(lblLayers);
 
         JLabel lblInputs = new JLabel("Inputs");
         lblInputs.setBounds(430, 40, 100, 45);
         frame.getContentPane().add(lblInputs);
 
         _inputTextArea = new JTextArea();
-        frame.add(_inputTextArea);
+        frame.getContentPane().add(_inputTextArea);
 
         JScrollPane jScrollPane = new JScrollPane(_inputTextArea);
         jScrollPane.setBounds(380,80,150,490);
-        frame.add(jScrollPane);
+        frame.getContentPane().add(jScrollPane);
 
         JLabel lblOutputs = new JLabel("Outputs");
         lblOutputs.setBounds(590, 40, 100, 45);
         frame.getContentPane().add(lblOutputs);
 
         _outputTextArea = new JTextArea();
-        frame.add(_outputTextArea);
+        frame.getContentPane().add(_outputTextArea);
 
         JScrollPane jScrollPane2 = new JScrollPane(_outputTextArea);
         jScrollPane2.setBounds(550,80,150,490);
-        frame.add(jScrollPane2);
-
-
-        _layersCount.getDocument().addDocumentListener(new DocumentListener() {
-            public void changedUpdate(DocumentEvent e) {
-                changeLayersCount();
-            }
-            public void removeUpdate(DocumentEvent e) {
-                changeLayersCount();
-            }
-            public void insertUpdate(DocumentEvent e) {
-                changeLayersCount();
-            }
-        });
-
-        _scrollPane = Box.createVerticalBox();
-        //_scrollPane.setBounds(10,80,500,500);
-        frame.getContentPane().add(_scrollPane);
-
-
-
-        _jScrollPane = new JScrollPane(_scrollPane);
-        _jScrollPane.setBounds(10,80,340,490);
-        //_jScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-        frame.getContentPane().add(_jScrollPane);
+        frame.getContentPane().add(jScrollPane2);
 
 //        _scrollPane.setBounds(10,80,500,500);
 //        _scrollPane.setHorizontalScrollBar(new JScrollBar(HORIZONTAL));
@@ -179,7 +227,7 @@ public class Window {
         });
 
         _btnChooseImportingFile = new JButton("Choose CSV file");
-        _btnChooseImportingFile.setBounds(380, 5, 200, 30);
+        _btnChooseImportingFile.setBounds(256, 5, 200, 30);
 
         _btnChooseImportingFile.addMouseListener(new MouseAdapter() {
             @Override
@@ -194,106 +242,120 @@ public class Window {
             }
         });
 
-        frame.add(_btnChooseImportingFile);
+        frame.getContentPane().add(_btnChooseImportingFile);
 
 
-        frame.add(_countButton);
+        frame.getContentPane().add(_countButton);
+
+        JLabel lblAlfa = new JLabel("Alfa");
+        lblAlfa.setBounds(0, 155, 114, 30);
+        frame.getContentPane().add(lblAlfa);
+
+        _alfa = new JTextField();
+        _alfa.setText("2");
+        _alfa.setBounds(50, 160, 50, 20);
+        frame.getContentPane().add(_alfa);
+
+        JLabel lblAlfa2 = new JLabel("Neighbourhood");
+        lblAlfa2.setBounds(0, 220, 114, 30);
+        frame.getContentPane().add(lblAlfa2);
+
+        _neighbourhood = new JTextField();
+        _neighbourhood.setText("2");
+        _neighbourhood.setBounds(130, 225, 50, 20);
+        frame.getContentPane().add(_neighbourhood);
+
+        JLabel lblIterationCount = new JLabel("Iteration count");
+        lblIterationCount.setBounds(0, 270, 114, 30);
+        frame.getContentPane().add(lblIterationCount);
+
+        _iterationCount = new JTextField();
+        _iterationCount.setText("2");
+        _iterationCount.setBounds(130, 275, 50, 20);
+        frame.getContentPane().add(_iterationCount);
+
+        JButton btnInitialize = new JButton();
+        btnInitialize.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                initializeNetwork();
+
+            }
+
+
+        });
+        btnInitialize.setText("Initialize");
+        btnInitialize.setBounds(479, 5, 100, 30);
+        frame.getContentPane().add(btnInitialize);
+    }
+
+    private void initializeNetwork() {
+        WeightsInitializer weightsInitializer;
+
+        if (_randomOrCustomWeights.getSelection().getActionCommand().equals("Random")){
+            weightsInitializer = new RandomWeightsInitializer(new Double(_minTextField.getText()), new Double(_maxTextField.getText()));
+        } else {
+            weightsInitializer = new ZeroWeightsInitializer();
+        }
+
+
+        _kohonenNetwork = new KohonenNetwork(new Integer(_inputCount.getText()),new Integer(_rowsTextField.getText()),
+                new Integer(_columnsTextField.getText()),weightsInitializer);
     }
 
     private void count() throws TooMuchInputLayersException, InvalidDimensionsException, IOException {
-        //Stringi bedace kolejnymi wejsciami
-        String[] inputsString = _inputTextArea.getText().split("\n");
-
-        //Tworzymy siec o ilosci wejsc rownej 2
-        NeuralNetwork nn = new NeuralNetwork(inputsString.length);
+        LearningParameters learningParameters = new LearningParameters();
+        learningParameters.setAlpha(new Double(_alfa.getText()));
+        learningParameters.setNeighborhood(new Integer(_neighbourhood.getText()));
+        _kohonenNetwork.setLearningParametrs(learningParameters);
+        _kohonenNetwork.setLearningMode(true);
 
         CSVReader reader = new CSVReader(new FileReader(_fileChoosenToImport));
 
-        int j = 0;
-        for(MyFrame myFrame : _myFrames){
-            //twrorzymy tablice neuronow w danej warstwie
-            NeuronData[] neuronDatas = new NeuronData[(int) myFrame.getNumberOfNeurons()];
 
-            //liczba neuronow w danej warstwie
-            int neuronNumber = (int) myFrame.getNumberOfNeurons();
 
-            System.out.println("j"+ j);
-            for( int i = 0 ; i < neuronNumber ; i++){
-                String [] nextLine = reader.readNext();
+        String [] nextLine = reader.readNext();
+        LinkedList<double[]> inputs = new LinkedList<double[]>();
+        //double[][] inputs = new double[][];
 
-                //czytamy bias
-                double bias = Double.parseDouble(nextLine[0]);
-
-                //tablica na wagi dla pojedynczego neuronu
-                double[] neuronWages = new double[nextLine.length-1];
-
-                //czytamy wagi
-                for (int k = 0 ; k < nextLine.length - 1; k++){
-                    neuronWages[k] = Double.parseDouble(nextLine[k+1]);
-                }
-
-                //zapisujemy do tablicy neuronow dany neuron
-                neuronDatas[i] = new NeuronData(bias , neuronWages);
+        while (nextLine != null){
+            double[] line = new double[nextLine.length];
+            for (int k = 0 ; k < nextLine.length; k++){
+                line[k] = Double.parseDouble(nextLine[k]);
             }
-
-            //tworzymy warstwe uzywajac funkcji aktywacji i tablicy neuronow
-            nn.addLayer(new NetworkLayer(myFrame.getActivationFunctionType(),neuronDatas));
-            j++;
+            inputs.add(line);
+            nextLine = reader.readNext();
         }
 
-        double[] inputs = new double[inputsString.length];
-        int i = 0;
-        System.out.println("wejscia: ");
-        for (String string : inputsString){
-            inputs[i] = Double.parseDouble(string);
-            System.out.println(inputs[i]);
-            i++;
+
+        for(int i=0;i< new Integer(_iterationCount.getText());i++){
+            for (double[] input: inputs){
+                _kohonenNetwork.setInputs(input);
+                _kohonenNetwork.calculate();
+            }
         }
 
-        System.out.println("inputs length: " + inputs.length);
-
-        nn.setInputs(inputs);
-        double[] outputs = nn.calculate();
+        System.out.println("After learning:");
+        _kohonenNetwork.setLearningMode(false);
 
 
-        String output = new String();
-        for(Double out : outputs){
-            output += out + "\n";
+        for (double[] input: inputs){
+            kohonenTest(_kohonenNetwork, input);
         }
-
-        _outputTextArea.setText(output);
-
-        System.out.println("wyjscie");
-        System.out.println(output);
-
     }
 
-    private void changeLayersCount(){
-        if (!_layersCount.getText().equals("")){
-            long layersCount = new Long(_layersCount.getText());
-            System.out.println(layersCount);
 
-            if (layersCount > _myFrames.size()){
-                //_scrollPane.setVisible(false);
-                int height = 1 + (_myFrames.size()) * 30;
-                MyFrame myFrame;
-                for (long i = _myFrames.size() ; i < layersCount ; i++){
-                    myFrame = new MyFrame(i,height);
-                    myFrame.setBounds(1, height, 320, 30);
-                    _scrollPane.add(myFrame);
-                    _myFrames.add(myFrame);
-                    height += 30;
-                }
-            } else {
-                System.out.println("asd");
-                System.out.println(_myFrames.size());
-                for (int i = _myFrames.size() - 1; i >= layersCount ; i--){
-                    _myFrames.get(i).setVisible(false);
-                    _myFrames.remove(i);
-                }
-            }
-            frame.revalidate();
+    private static void kohonenTest(KohonenNetwork kohonenNetwork, double[] inputs) throws InvalidDimensionsException {
+        StringBuilder sb = new StringBuilder();
+        for(double input: inputs){
+            sb.append(input).append("  ");
+        }
+        System.out.println("Testing Kohonen with input " + sb.toString());
 
+        kohonenNetwork.setInputs(inputs);
+        double[] outputs = kohonenNetwork.calculate();
+        for(double output : outputs){
+            System.out.println(output);
         }
     }
 }
